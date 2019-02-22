832bc12388f361c2f0b5c8c55ec3082dcbbba5fd
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-02-22 14:51:50 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,22 +1,12 @@
-import java.util.Scanner;
-
-public class TicTacToe {
+public class Board {
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-		int a = 1;
-		int n = 0;
-		Boolean player1 = false;
-		Boolean player2 = false;
-		Boolean positionMessage = false;
-		Boolean quadFound = false;
-		String answer;
-		String player;
+	
 		String quadrants[] = { "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11", "Q12", "Q13",
 				      "Q14", "Q15", "Q16", "Q17", "Q18", "Q19", "Q20", "Q21", "Q22" , "Q23", "Q24", "Q25"
 				     };
-		String targetQuads[] = { "*", "*", "*", "*", "*",  "*", "*", "*", "*", "*",  " ", " ", " ", " ", " ",  " ", " ", "Shield ", " ", " ",  " ", "Shield ", " ", " ", " ",};
+		String targetQuads[] = { "*", "*", "*", "*", "*",  "*", "*", "*", "*", "*",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",};
 
 		System.out.println("Space Invaders");
 			
