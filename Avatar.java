cf7e27f94940891e3c07b2b348b5f41b24897f30
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-02-22 14:56:47 -0700

    Update Avatar.java

--- Avatar.java
+++ Avatar.java
@@ -1,28 +1,28 @@
-import java.awt.Rectangle
+import java.awt.Rectangle;
 
 public class Avatar {
   
   private String name;
   private char sprite;
   private int lives;
-  private Rectangle location;
+  private Rectangle location;;
 	
 	public Avatar(){
 	name = "a";
 	sprite = 'o';
-	lives = 3
-	location = (2,4)
+	lives = 3;
+	location = new Rectangle(2,4);
 	}
 	
-  public void moveRight(){
-		location.width += 1
-  }
+	public void moveRight(){
+		location.width += 1;
+	}
 	
 	public void moveLeft(){
-		location.width -= 1
+	location.width -= 1;
 	}
   
   public Rectangle getLocation(){
-		return location;
+	return location;
   }
 } 
