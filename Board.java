6689265b9472b311f27915ea14620dfaf688989e
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-02-22 12:24:28 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,3 +1,10 @@
 public class Board {
+ char[][] grid;
+	
+	public static void main(String[] args) {
+		char[][] startConfiguration = new char[3][3];
+		for (int row = 0; row < 3; row++) {
+			for (int column = 0; column < 3; column++) {
+				startConfiguration[row][column] = args[row*3+column].charAt(0)
  
  }
