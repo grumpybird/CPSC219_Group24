f1b82df775c649d7393a9dcdc233baa0fdf71739
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-02-22 12:45:57 -0700

    Update Group_Project.java

--- Group_Project.java
+++ Group_Project.java
@@ -1,7 +1,21 @@
 public class Group_Project {
  
- int totalWidth = 500;
+ int row;
+ int column;
   
+  public  void newBoard()
+    {
+        
+        char posndef[] = {'','', '', '', '', '', '', '', '', ''};
+        int i;
+        counter = 0;
+        player = 'X';
+        for (i=1; i<10; i++) posn[i]=posndef[i];
+        currentBoard();
+        
+        
+    }
+ 
  }
 
 
