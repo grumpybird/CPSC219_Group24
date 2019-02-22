2fb2ea1dd74886c8a9860f66060d25ef3667974a
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-02-22 14:43:01 -0700

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -4,7 +4,30 @@ public class SpaceInvaders{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Move character (left or right)");
-    input = input.nextLine();
+      input = input.nextLine();
+    while input != 'x';
+    {
+      System.out.println("Move character (left or right)");
+      input = input.nextLine();
+        if (input == 'a')
+        {
+          avatar.moveLeft;
+        } 
+       if (input == 'd')
+        {
+          avatar.moveRight;
+        }
+      if (input == 'x')
+        {
+          break;
+        }
+        
+      else 
+      {
+        system.out.println("Invalid input, try again");
+        input = input.nextLine();
+      }
+    }
     
                  
   
