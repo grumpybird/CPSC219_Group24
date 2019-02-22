001718cf084d04a5de7ca6ed7ac431b5a89f285d
Paula Almeida <47282849+pnalmeida@users.noreply.github.com>
2019-02-22 14:48:55 -0700

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class SpaceInvaders{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Move character (left (a) or right (d) or x to exit)");
+    System.out.println("Move character (left 'a' or right 'd') or 'x' to quit game.");
       input = input.nextLine();
     while input != 'x';
     {
-      System.out.println("Move character (left or right)");
+      System.out.println("Move character (left 'a' or right 'd') or 'x' to quit game.");
       input = input.nextLine();
         if (input == 'a')
         {
-          avatar.moveLeft;
+          avatar.moveLeft();
         } 
        if (input == 'd')
         {
-          avatar.moveRight;
+          avatar.moveRight();
         }
       if (input == 'x')
         {
