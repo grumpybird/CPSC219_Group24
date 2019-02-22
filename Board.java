63c583256e758a92fadd2283091a384acd442a6a
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-02-22 14:32:22 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -13,8 +13,10 @@ public class Board {
 		Boolean quadFound = false;
 		String answer;
 		String player;
-		String quadrants[] = { "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11", "Q12" };
-		String targetQuads[] = { " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",};
+		String quadrants[] = { "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11", "Q12", "Q13",
+				      "Q14", "Q15", "Q16", "Q17", "Q18", "Q19", "Q20", "Q21", "Q22" , "Q23", "Q24", "Q25"
+				     };
+		String targetQuads[] = { "*", "*", "*", "*", "*",  "*", "*", "*", "*", "*",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",  " ", " ", " ", " ", " ",};
 
 		System.out.println("---*** TIC-TAC-TOE ***---");
 		while (a <= 9) {
