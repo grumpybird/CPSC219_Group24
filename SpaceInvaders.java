8f9e4e7d81249f0662d642a299cbeaf8d665cc82
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-02-22 14:35:35 -0700

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class SpaceInvaders{
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Move character (left or right)");
+    input = input.nextLine();
     
+                 
   
   }
 }
