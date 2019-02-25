fe984ddab3d01506c7031027e1c515fea07b9e45
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-02-25 10:51:58 -0700

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class SpaceInvaders {
-  public static void main(String[] args){
+public class SpaceInvaders2 {
+	
+	
+  public static void main(String[] args) {
 	System.out.println("       Space Invaders     ");
+	
+	String targetQuads[] = { "*", "*", "*", "*", "*",  "*", "*", "*", "*", "*",  " ", " ", " ", " ", " ",  "-", " ", "-", " ", "-", " ", " ", "X ", " ", " ",};
+	
+	int i;
+	for (i = 0; i < targetQuads.length; i++) {
+		System.out.print(targetQuads[i] + "          ");
+		if ((i + 1) % 5 == 0)
+			System.out.println("");
+	}
+	
     Avatar a = new Avatar();
     Board b = new Board();
     Scanner input = new Scanner(System.in);
     String sc = "";
-    
+  
     while ((sc.equals("x") == false))
     {
     	System.out.println("Move character (left 'a' or right 'd') or 'x' to quit game.");
@@ -15,11 +28,11 @@ public class SpaceInvaders {
         
         if (sc.equals("a"))
         {
-          //a.moveLeft();
+          a.moveLeft();
         } 
         else if (sc.equals("d")) 
         {
-          //a.moveRight();
+          a.moveRight();
         }
         else if (sc.equals("x")) 
         {
@@ -39,3 +52,5 @@ public class SpaceInvaders {
   
   }
 }
+
+
