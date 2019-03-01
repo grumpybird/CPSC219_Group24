311299b56397cd44bfeca3214592eccf351fe467
Paula Almeida <47282849+pnalmeida@users.noreply.github.com>
2019-02-28 21:01:18 -0700

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -1,10 +1,36 @@
-/**
-*/
+ /**
+ * CPSC 219 Group 24 Project: Board Class
+ * 
+ * @author: Paula Almeida
+ *	    Gabriel Atienza
+ *	    Sharon Chan
+ *	    Hayden Kerr
+ *	    Theresa Lam
+ *
+ * @date: March 1, 2019.
+ *
+ * @version: DEMO 1
+ *
+ * URL OF CODE USED AND MODIFIED TO DEVELOP THIS PART OF THE PROGRAM:
+ * https://gist.github.com/xSputnicKx/fbf9a329818cff0a4a02f773b22ed9a7?fbclid=IwAR0ojil66TCbkFd4DlJT5zh35pDCxidxa_RFCssuYXWBCaEO2bDXCSfzGk8
+ *
+ */
 
 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+
+/**
+ * FOR DEMO 1:
+ * Code is organized so that everything runs from the SpaceInvaders.java file
+ * Code and methods used from other classes to form a composite 
+ * version of the program that relies on itself.
+ * Takes over for the game's Board class, Avatar class, Obstacles class, etc.
+ *
+ */
+
+
 
 /**
  * This class represents a game. It launches the game and quits the game. 
