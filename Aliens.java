c327f8ed6b16ffd4982e2b30879a5d30ee81a06b
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-03-11 12:57:28 -0600

    Update Aliens.java

--- Aliens.java
+++ Aliens.java
@@ -3,12 +3,19 @@ Use this class for the behavior of the aliens. I think the best way to do this w
 which has the aliens move between rows and columns at each interval. Timer will also have them shoot, will probably need an if loop
 to determine which aliens are on the bottom row, and are therefore permitted to shoot
 #####################################################################################################################################*/
-import javafx.application.Application;
-import javafx.scene.image
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;  
+import javafx.stage.Stage;
 
 public class Aliens extends Application{
-  FileInputStream inputstream = new FileInputStream("alien.jpg");
-  Image image = new Image(inputstream); 
+  
+   //Creating an image 
+   Image alien = new Image(new FileInputStream("alien.jpg"))
+   
+   //
   
   
 }
