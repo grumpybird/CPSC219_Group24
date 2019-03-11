6f8d021b080aeaf3ae87e2ac333cb57066cae8f6
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-03-10 23:27:55 -0600

    created class, added notes and an instance variable

---
+++ Projectile.java
@@ -0,0 +1,12 @@
+/*#####################################################################################################################################
+This class will have all of the events for projectiles if possible. Their properties including speed and determining how they will move
+across the board. Lots of if statements to determine which object they are colliding with and events for how they will interact
+with the object theyve collided with, ie losing a life or removing an alien from the board. Consider using events so that if the Projectile
+class determines an instance of itself has collided with an object of another class, call a method from that class which will contain
+the appropriate action
+#####################################################################################################################################*/
+
+public class Projectile{
+  
+  public final int SPEED;
+}
