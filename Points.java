de90180728f021bfc1aeb11852f2d63f06f3b590
theresa-lam <47282780+theresa-lam@users.noreply.github.com>
2019-03-11 12:47:54 -0600

    Update Points.java

--- Points.java
+++ Points.java
@@ -1,5 +1,7 @@
 
 /**
+A class that increases the points everytime there is a collision event between the projectile from the avatar and the aliens
+
 */
 public class Points {
   private int level = 1;
