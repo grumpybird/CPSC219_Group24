9ba142af0fa67eb9e18ba3eb35c839bf64102ae6
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-03-11 12:36:56 -0600

    Update Obstacles.java

--- Obstacles.java
+++ Obstacles.java
@@ -13,15 +13,14 @@ a potential projectiles class
  *	        Hayden Kerr
  *	        Theresa Lam
  *
- * @date: March 1, 2019.
+ * @date: March 11, 2019.
  *
- * @version: DEMO 1
+ * @version: DEMO 2
  *
  */
 
 /**
- * Class represents game obstacles such as shields.
- * Not developed for demo 1.
+ * Class represents game obstacles such as barriers.
  * Will vary in number and location on the game board
  * based on the level.
  * Attributes will include:
@@ -30,5 +29,48 @@ a potential projectiles class
  *
  */
 public class Obstacles {
+  private int barrierHealth = 0;
+  private int counter = 0;
+  private ArrayList<Barrier> barriers = new ArrayList<>();
+
+  
+  public void setBarriersLocation() {
+    // CALLS BOARD TO FIND LOCATION 
+    
+  }
+  
+  public getBarrierHit() {
+  }
+  
+  public void setbarrierHealth(int barrierHealth) {
+    while counter <= 12 
+      if (b.collision = true)
+      { counter += 1; 
+      } 
+  }
+  
+  public getbarrierHealth() {
+    return barrierHealth;
+  }
+  
+  public void checkBarrierCollision(Barrier barrier) {
+     for (int b = 0; b < barrier.getBarrierHit(); b++)
+          barrier.getProjectile(b).detectCollision(alien);
+    
+     if (barrier.isActive() == true && alien.isActive())
+                {
+                    barrier.detectCollision(alien);
+                }   
+            }
+            else if (alien.isActive() == false)
+            {
+                totalscore = totalscore + avatar.GetScore();
+            }
+        }
+
+
+
+      
+  } 
 
 }
