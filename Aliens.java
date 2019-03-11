bf55c7f7a534b04c35ada5fa05a2ee6d525b4b64
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-03-10 23:23:37 -0600

    Create Aliens.java
    added notes

---
+++ Aliens.java
@@ -0,0 +1,9 @@
+/* ####################################################################################################################################
+Use this class for the behavior of the aliens. I think the best way to do this would be with a timer (nested for loops) and a gridbox,
+which has the aliens move between rows and columns at each interval. Timer will also have them shoot, will probably need an if loop
+to determine which aliens are on the bottom row, and are therefore permitted to shoot
+#####################################################################################################################################*/
+
+public class Aliens{
+
+}
