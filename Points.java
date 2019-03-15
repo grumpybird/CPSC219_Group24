2e529738909bc2ff864c32e4b21fcbd63cd17b5a
theresa-lam <47282780+theresa-lam@users.noreply.github.com>
2019-03-14 21:09:19 -0600

    Update Points.java

--- Points.java
+++ Points.java
@@ -17,10 +17,15 @@ public class Points extends SpaceInvaders{
 has hit the ship and increases the score. for simplicity our current model will increase 1 point for each alient hit.
 we plan to have an increase in score as the aliens get farther
 */
+  
 public void setScore(){
  // if (alien.collision() = true)
    // score += 1;
 }
+/** 
+*Return Score
+*/
+
 public getScore(){
   return score;
 }
