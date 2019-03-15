e119b2f7d897daeecd83ef846c35086faac58bf0
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-03-14 21:02:02 -0600

    Update Avatar.java

--- Avatar.java
+++ Avatar.java
@@ -9,7 +9,7 @@
  *
  * @date: March 1, 2019.
  *
- * @version: DEMO 1
+ * @version: DEMO 2
  *
  */
 
@@ -34,7 +34,7 @@ import javafx.scene.image.Image;
  * Class represents the avatar in the game.
  * Avatar class controls the movement of the 
  * avatar as well as attributes such as:
- * name, lives, position, and appearance(not in demo 1).
+ * name, lives, position, and appearance.
  *
  * This class only focuses on the movement and health of the
  * avatar. State of health will be updated from classes in the game
@@ -43,6 +43,7 @@ import javafx.scene.image.Image;
  */
 public class Avatar {
 
+	// Instance variables
  	private static final double INITIAL_AVATAR_X = 362.5;
  	private static final double AVATAR_Y = 550;
  	private static final int AVATAR_VELOCITY_X = 14;
@@ -54,6 +55,7 @@ public class Avatar {
     private int lives;
     private double locationX;
 
+	// Constructor
 	public Avatar(){
         double locationY = AVATAR_Y;
         int velocityX = AVATAR_VELOCITY_X;
@@ -66,11 +68,14 @@ public class Avatar {
         locationX = INITIAL_AVATAR_X;
 	}
 
+	// drawAvatar method uses the GraphicsContext to draw the sprite for the avatar, and overwrite the previous
+	// Instance of the sprite
 	public void drawAvatar(GraphicsContext gc){
         gc.fillRect(0, 550, 800, 50);
         gc.drawImage(SPRITE, locationX, AVATAR_Y, SPRITE_WIDTH, SPRITE_HEIGHT);
     }
 
+	// moveLeft method moves the avatar a given distance to the left, so long as it is not at the left edge of the board
 	public void moveLeft(){
 	    if (locationX - AVATAR_VELOCITY_X >= 0) {
             locationX -= AVATAR_VELOCITY_X;
@@ -80,6 +85,7 @@ public class Avatar {
         }
 	}
 
+	// moveLeft method moves the avatar a given distance to the right, so long as it is not at the right edge of the board
 	public void moveRight(){
 	    if (locationX + AVATAR_VELOCITY_X <= 725){
             locationX += AVATAR_VELOCITY_X;
@@ -89,6 +95,7 @@ public class Avatar {
         }
 	}
 
+	// Getter methods
 	public double getLocationX(){
 		return locationX;
 	}
