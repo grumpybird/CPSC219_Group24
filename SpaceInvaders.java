4f7fb31e8ef152d65529d9c9cf88a0516e73e0dd
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-03-14 20:02:29 -0600

    Update SpaceInvaders.java

--- SpaceInvaders.java
+++ SpaceInvaders.java
@@ -16,8 +16,10 @@
  *
  */
 
-
+import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
@@ -33,13 +35,18 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.event.EventHandler;
-
+import javafx.util.Duration;
 import java.util.HashSet;
 
 public class SpaceInvaders extends Application {
-
+    
+     Group root = new Group();
+    
+    boolean rightEnemy = true;
+	 public int MOV = 0;
     private static final double BOARD_WIDTH = 800;
     private static final double BOARD_HEIGHT = 600;
 
@@ -50,7 +57,40 @@ public class SpaceInvaders extends Application {
     private static Obstacles barrier1;
     private static Obstacles barrier2;
     private static Obstacles barrier3;
-
+    
+    Image enemiesV = new Image(
+            "alien.png");
+    int SCREEN_WIDTH = 800;
+    int SCREEN_HEIGHT = 800;
+    int ENEMY_EDGE = 40;
+    int ENEMY_ROW = 5;
+    int ENEMY_COLUMN = 7;
+    int SPEED = 3;
+    
+    static Rectangle pointer = new Rectangle();
+    
+    ImageView[] enemies = new ImageView[ENEMY_COLUMN * ENEMY_ROW];
+    int updateTime = 28;
+    
+    public void movement() {
+        for (int j = 0; j < ENEMY_ROW ; j++) {
+            for (int i = 0; i < ENEMY_COLUMN; i++) {
+                enemies[j * ENEMY_COLUMN + i] = new ImageView(enemiesV);
+                enemies[j * ENEMY_COLUMN + i].setPreserveRatio(true);
+                enemies[j * ENEMY_COLUMN + i].setX(i * 50);
+                enemies[j * ENEMY_COLUMN + i].setY(j * 50);
+                enemies[j * ENEMY_COLUMN + i].setFitWidth(ENEMY_EDGE);
+                root.getChildren().add(enemies[j * ENEMY_COLUMN + i]);
+                if (i == ENEMY_COLUMN - 1 && j == 0) {
+                    pointer.setWidth(ENEMY_EDGE);
+                    pointer.setHeight(ENEMY_EDGE);
+                    pointer.setFill(Color.TRANSPARENT);
+                    pointer.setX(enemies[i].getX() + ENEMY_EDGE);
+                }
+            }
+        }
+        updateTime-=3;
+    }
     public static void main(String[] args){
         launch(args);
     }
@@ -59,7 +99,7 @@ public class SpaceInvaders extends Application {
 
         primaryStage.setTitle("Space Invaders");
 
-        Group root = new Group();
+  
         boardScene = new Scene(root, BOARD_WIDTH, BOARD_HEIGHT);
         primaryStage.setScene(boardScene);
 
@@ -78,6 +118,13 @@ public class SpaceInvaders extends Application {
         gc.fillText("LIVES: ", 100, 25);
         gc.fillText("SCORE: ", 500, 25);
 
+        movement();
+        
+        Duration dI = new Duration(updateTime);
+        KeyFrame f = new KeyFrame(dI, e -> movementCore());
+        Timeline tl = new Timeline(f);
+        tl.setCycleCount(Animation.INDEFINITE);
+        tl.play();
         
         barrier1 = new Obstacles(1);
         barrier2 = new Obstacles(2);
@@ -149,4 +196,30 @@ public class SpaceInvaders extends Application {
         }
     }
 
+     public void movementCore() {
+        if (rightEnemy) { //check if the enemy is going toward right
+            if (pointer.getX() + ENEMY_EDGE >= BOARD_WIDTH) { //check collision on right edge
+                rightEnemy = false;
+
+            }
+            for (int i = 0; i < enemies.length; i++) {
+                if (enemies[i] != null) {
+                    enemies[i].setX(enemies[i].getX() + SPEED); //move the enemy 
+                }
+            }
+            pointer.setX(pointer.getX() + SPEED); //move the pointer
+        } else {
+            if (pointer.getX() - ((ENEMY_EDGE * (ENEMY_COLUMN + 2))) <= 0) {
+                rightEnemy = true;
+            }
+            for (int i = 0; i < enemies.length; i++) {
+                if (enemies[i] != null) {
+                    enemies[i].setX(enemies[i].getX() - SPEED);
+                }
+            }
+            pointer.setX(pointer.getX() - SPEED);
+        }
+     
+       
+    }
 }
