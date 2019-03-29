00987e78dffb40af6f316378e45a0eaa05fbae82
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-03-29 00:30:00 -0600

    Update SpaceInvadersJunitTest.java

--- SpaceInvadersJunitTest.java
+++ SpaceInvadersJunitTest.java
@@ -1,3 +1,24 @@
+/* CPSC 219 Group 24 Project: Testing Points in SpaceInvader
+*
+* @author: Paula Almeida
+* 	   Gabriel Atienza
+*	   Sharon Chan
+*	   Hayden Kerr
+*	   Theresa Lam
+*
+* @date: March 29, 2019.
+*
+* @version: DEMO 3
+*
+*/
+
+/**
+* Testing the points in SpaceInvader.
+* Where the points are being tested against a limit 
+* and find and see if it reaches a negative score. And 
+* if it does it will give an error
+*/ 
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
