58cce94892682d9e5d14938ba03b7eaf23145fa1
theresa-lam <47282780+theresa-lam@users.noreply.github.com>
2019-04-11 17:03:15 -0600

    Update Bullets.java

--- SpaceInvaders/Bullets.java
+++ SpaceInvaders/Bullets.java
@@ -1,45 +1,28 @@
-import java.util.Collections;
+public class Bullets extends SpaceInvadersT{
 
-public class Obstacles extends SpaceInvadersT{
-	
-public Obstacles () {
-	super(getLocation(), getTargetQuads(), getPoints());
-	}
-
-public static void moveRight() {
-	if (getLocation() < 62) {
-		Collections.swap(myList, getLocation(), getLocation() + 1);
-		location = location + 1;
-		System.out.println ("                            points:" + getPoints());
-			for (int i = 0; i < myList.size(); i++) {
-				System.out.print(myList.get(i) + "          ");
-					if ((i + 1) % 7 == 0){
-						System.out.println("");
-					}
-			}	
-	
-	}
-	else 
-		System.out.println("Sorry can't move farther right.");
-	
-}
-public static void moveLeft () {
-	if (getLocation() > 56) {
-		Collections.swap(myList, getLocation(), getLocation() - 1);
-		location = location - 1;
-		System.out.println ("                            points:" + getPoints());
-		for (int i = 0; i < myList.size(); i++) {
-			System.out.print(myList.get(i) + "          ");
-			if ((i + 1) % 7 == 0){
-				System.out.println("");
-			}
-		}
-	
-	}
-	else 
-		System.out.println("Sorry can't move farther left.");	
-	
+public Bullets () {
+super(getLocation(), getTargetQuads(), getPoints());
 }
 
-
+public static void fire() {
+	int count = 1;
+	for (count = 1; count<=8 ;count++) {
+	 if (getTargetQuads()[(getLocation() - 7*(count))].equals (" ") == false){
+		 getTargetQuads()[(getLocation() - 7*count)] = " ";
+  		myList.set((getLocation() - 7*(count)), " ");
+  		
+  		break;	
+	 }
+	}
+	System.out.println ("                            points:" + getPoints());
+  	for (int i = 0; i < myList.size(); i++) {
+  		System.out.print(myList.get(i) + "          ");
+  		if ((i + 1) % 7 == 0){
+		System.out.println("");
+				}
+  			}
+  	if (myList.contains("*") == false) {
+  		System.out.println("YOU WON!!!");
+  	}
+}
 }
