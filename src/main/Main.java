baf51dd56dabed240e1e12d73dfb9c0573fc9f78
haydenkerr1 <47900367+haydenkerr1@users.noreply.github.com>
2019-04-11 19:02:47 -0600

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,50 @@
+package main;
+
+import core.ScreenController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import core.SpaceInvaders;
+import core.StartScreen;
+
+public class Main extends Application {
+
+    Stage stage;
+    private Pane pane = new Pane();
+
+    StartScreen startscreen = new StartScreen();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        stage = primaryStage;
+        Scene scene = startscreen.getBoardScene();
+        ScreenController screenController = new ScreenController(scene);
+
+        screenController.addScreen("boardscene", pane);
+        screenController.addScreen("startScene", pane);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        scene.setOnKeyPressed(e -> keyboardManage(e, screenController, primaryStage));
+
+    }
+
+    public void keyboardManage(KeyEvent ke, ScreenController sc, Stage primaryStage) {
+        if (ke.getCode() == KeyCode.ENTER) {
+            SpaceInvaders boardscreen = new SpaceInvaders();
+            sc.activate("boardscene");
+            Scene scene = boardscreen.GameScreen();
+            primaryStage.setScene(scene);
+            scene.setOnKeyPressed(e -> boardscreen.keyboardManager(e));
+        }
+    }
+}
