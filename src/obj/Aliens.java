3e615871268f23b175bbd65898180fc33e69f245
theresa-lam <47282780+theresa-lam@users.noreply.github.com>
2019-04-11 23:41:32 -0600

    Update Aliens.java

--- src/obj/Aliens.java
+++ src/obj/Aliens.java
@@ -1,3 +1,16 @@
+/**
+ * CPSC 219 Group 24 Project: Alien Class
+ *
+ * @author: Paula Almeida
+ *	        Gabriel Atienza
+ *	        Sharon Chan
+ *	        Hayden Kerr
+ *	        Theresa Lam
+ *
+ * @date: April 11th, 2019.
+ * @version: Alien class of Space Invaders GUI Game for interactive demo.
+ */
+
 package obj;
 
 import javafx.scene.paint.Color;
@@ -10,36 +23,30 @@ import javafx.geometry.Rectangle2D;
 import main.Main;
 
 
-//implement Aliens in the main app under stage to implement variables, then call constructor
+/**
+* implement Aliens in the main app under stage to implement variables, then call constructor
+*/
 public class Aliens {
     //instance variables
-    private static Image enemiesV = new Image(
-            "res/alien1-2.gif");
-
+    private static Image enemiesV = new Image("res/alien1-2.gif");
     private static boolean rightEnemy = true;
     public int MOV = 0;
-
-  
      private static int ENEMY_EDGE = 40;
      private static int ENEMY_ROW = 5;
      private static int ENEMY_COLUMN = 7;
      private static int SPEED = 3;
      public static int score = 0;
-     
      private static double Sprite_BoardW = 800;
-    
      static Rectangle pointer = new Rectangle();
-   
-    
-
-
      public static ImageView[] enemies = new ImageView[ENEMY_COLUMN * ENEMY_ROW];
      public int updateTime = 65;
 
     public Rectangle2D getBoundary(){
         return new Rectangle2D(1, 1, 1, 1);
     }
-
+    /**
+    * This movement class controls the movements of the aliens in the main class.
+    */
     public void movement(Pane hbox) {
         for (int j = 0; j < ENEMY_ROW ; j++) {
             for (int i = 0; i < ENEMY_COLUMN; i++) {
@@ -60,8 +67,12 @@ public class Aliens {
         updateTime-=3;
     }
 
-
-
+    /**
+    * As the game progresses the aliens will move side to side, and moving down when they reach the edge.
+    * the class movementCore keeps track of where the enemies are located
+    * checking for collsions with the edge of the screen.
+    * WHen there is a collsion with the edge, the enemy will movedown.
+    */ 
     public void movementCore(Bullet bulletc, Text punt, boolean newLevel, Pane pane) {
         if (rightEnemy) { //check if the enemy is going toward right
             if (pointer.getX() + ENEMY_EDGE >= Sprite_BoardW) { //check collision on right edge
