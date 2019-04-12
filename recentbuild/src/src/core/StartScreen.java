0aba300826929f481be0e9894bb4d37a0a2186dc
Gabriel Anthony Atienza <ganciel.ga@gmail.com>
2019-04-12 14:40:12 -0600

    most recent one, download as package

---
+++ recentbuild/src/src/core/StartScreen.java
@@ -0,0 +1,104 @@
+/**
+ * CPSC 219 Group 24 Project: StartScreen Class
+ * 
+ * @author: Paula Almeida
+ *	    Gabriel Atienza
+ *	    Sharon Chan
+ *	    Hayden Kerr
+ *	    Theresa Lam
+ *
+ * @date: April 11th, 2019.
+ * @version: StartScreen class of Space Invaders GUI Game for interactive demo.
+ */
+
+package src.core;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.layout.Pane;
+/**
+ * 
+ * The class StartScreen creates scene when the application is called.
+ * It then adds the various components
+ *
+ */
+public class StartScreen{
+	//Instance Variables
+    private Group root = new Group(); 
+    private Pane pane = new Pane();
+    //Set board width and height
+    private static final double BOARD_WIDTH = 800;
+    private static final double BOARD_HEIGHT = 800;
+    private static Scene startScene;
+    private static GraphicsContext gc;
+	
+// Method
+    public Scene getBoardScene() {
+
+    	startScene = new Scene(root, BOARD_WIDTH, BOARD_HEIGHT);  //set new scence
+        Canvas canvas = new Canvas(BOARD_WIDTH, BOARD_HEIGHT); // set new canvas
+        
+        root.getChildren().add(canvas); //add canvas to root
+        root.getChildren().add(pane); // add pane to root
+        
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLACK); // Set color to black
+        gc.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT); // make background balck
+
+        gc.setFill(Color.LIMEGREEN); // change color to lime green
+        
+        Font spaceinvaders = Font.font("Courier", 14); // setting font and style
+        gc.setFont(spaceinvaders);
+        gc.fillText( "                          _                     _               \n" + 
+        		"                         (_)                   | |              \n" + 
+        		" ___ _ __   __ _  ___ ___ _ _ ____   ____ _  __| | ___ _ __ ___ \n" + 
+        		"/ __| '_ \\ / _` |/ __/ _ \\ | '_ \\ \\ / / _` |/ _` |/ _ \\ '__/ __|\n" + 
+        		"\\__ \\ |_) | (_| | (_|  __/ | | | \\ V / (_| | (_| |  __/ |  \\__ \\\n" + 
+        		"|___/ .__/ \\__,_|\\___\\___|_|_| |_|\\_/ \\__,_|\\__,_|\\___|_|  |___/\n" + 
+        		"    | |                                                         \n" + 
+        		"    |_|                                                         ", 100, 50); 
+        
+        Font group24 = Font.font("Courier", 8); //change font size
+        gc.setFont(group24);
+        gc.fillText("                                                                   \n" + 
+        		"  ,ad8888ba,  88888888ba    ,ad8888ba,   88        88 88888888ba   \n" + 
+        		" d8\"'    `\"8b 88      \"8b  d8\"'    `\"8b  88        88 88      \"8b  \n" + 
+        		"d8'           88      ,8P d8'        `8b 88        88 88      ,8P  \n" + 
+        		"88            88aaaaaa8P' 88          88 88        88 88aaaaaa8P'  \n" + 
+        		"88      88888 88\"\"\"\"88'   88          88 88        88 88\"\"\"\"\"\"'    \n" + 
+        		"Y8,        88 88    `8b   Y8,        ,8P 88        88 88           \n" + 
+        		" Y8a.    .a88 88     `8b   Y8a.    .a8P  Y8a.    .a8P 88           \n" + 
+        		"  `\"Y88888P\"  88      `8b   `\"Y8888Y\"'    `\"Y8888Y\"'  88           \n" + 
+        		"                                                                   \n" + 
+        		"                                                                   \n" + 
+        		"                        \n" + 
+        		" ad888888b,      ,d8    \n" + 
+        		"d8\"     \"88    ,d888    \n" + 
+        		"        a8P  ,d8\" 88    \n" + 
+        		"     ,d8P\" ,d8\"   88    \n" + 
+        		"   a8P\"  ,d8\"     88    \n" + 
+        		" a8P'    8888888888888  \n" + 
+        		"d8\"               88    \n" + 
+        		"88888888888       88    ", 100, 300);
+        
+        Font alien = Font.font("Courier", 8); // change font size
+        gc.setFont(alien);
+        gc.fillText("░░░░░░░░░░░░░░░░░\n" + 
+        		"░░░▄░▀▄░░░▄▀░▄░░░\n" + 
+        		"░░░█▄███████▄█░░░\n" + 
+        		"░░░███▄███▄███░░░\n" + 
+        		"░░░▀█████████▀░░░\n" + 
+        		"░░░░▄▀░░░░░▀▄░░░░\n" + 
+        		"░░░░░░░░░░░░░░░░░", 100, 500);
+
+       return startScene;
+    }
+   
+}
+
+
+                                                    
---
+++ recentbuild/src/src/main/Main.java
@@ -0,0 +1,88 @@
+/**
+ * CPSC 219 Group 24 Project: Main Class
+ *
+ * @author: Paula Almeida
+ *	    Gabriel Atienza
+ *	    Sharon Chan
+ *	    Hayden Kerr
+ *	    Theresa Lam
+ *
+ * @date: April 11th, 2019.
+ * @version: Main class of Space Invaders GUI Game for interactive demo.
+ */
+
+package src.main;
+
+import src.core.ScreenController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import src.core.SpaceInvaders;
+import src.core.StartScreen;
+import src.obj.EnemyBullet;
+
+public class Main extends Application {
+
+    // Instance variables
+    private static Stage stage;
+    private Pane pane = new Pane();
+
+    StartScreen startscreen = new StartScreen();
+
+    // Main method
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * @param primaryStage of type stage
+     * Opens up application and displays start screen
+     *
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        stage = primaryStage;
+        Scene scene = startscreen.getBoardScene();
+        ScreenController screenController = new ScreenController(scene);
+
+        screenController.addScreen("boardscene", pane);
+        screenController.addScreen("startScene", pane);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        scene.setOnKeyPressed(e -> keyboardManage(e, screenController, primaryStage));
+
+    }
+
+    //Keyboard Manager
+    /**
+     *
+     * @param ke of type KeyEvent
+     * @param sc of type ScreenController
+     * @param primaryStage of type Stage
+     *
+     * When the user presses enter, switches stages to start game.
+     */
+    public void keyboardManage(KeyEvent ke, ScreenController sc, Stage primaryStage) {
+        if (ke.getCode() == KeyCode.ENTER) {
+            SpaceInvaders boardscreen = new SpaceInvaders();
+            sc.activate("boardscene");
+            Scene scene = boardscreen.GameScreen();
+            primaryStage.setScene(scene);
+            scene.setOnKeyPressed(e -> boardscreen.keyboardManager(e));
+        }
+    }
+
+    public static void closeStage(Stage primaryStage){
+        primaryStage.close();
+    }
+
+    public static Stage getStage(){
+        return stage;
+    }
+}
