0c6bd20beb87a94458ae55a5356d597b063137ec
nahcnorahs <47257668+nahcnorahs@users.noreply.github.com>
2019-04-12 21:44:46 -0600

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,7 +22,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import core.SpaceInvaders;
 import core.StartScreen;
-import obj.Aliens;
+import obj.EnemyBullet;
 
 public class Main extends Application {
 
